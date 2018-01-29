c68d0570beee05e8781a0f3a7c831ba18a99942a
LAPTOP-8PEB5H4U\minja <mta305@nyu.edu>
2018-01-29 14:53:27 +0800

    create-react-app test
    node and react installed

---
+++ test/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <p> I have created the test </p>
+    );
+  }
+}
+
+export default App;
